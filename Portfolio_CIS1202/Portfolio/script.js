4af87ab3297d6da61b42f5e5cf6b3946e906c05f
Direl Christian Aguinaldo <bluejiofficial@gmail.com>
2026-03-16 15:16:16 +0800

    Added transitions in CSS

---
+++ Portfolio_CIS1202/Portfolio/script.js
@@ -0,0 +1,6 @@
+// Function to toggle dark mode
+function toggleDarkMode() {
+    // Access the body element and toggle the 'dark-mode' class
+    document.body.classList.toggle('dark-mode');
+    console.log("Dark mode toggled!"); 
+}
